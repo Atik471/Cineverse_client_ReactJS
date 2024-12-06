18ff68995b8f53c653f624e148f83cc772954efc
Atik471 <108218772+Atik471@users.noreply.github.com>
2024-12-06 23:10:35 +0600

    added 404 design

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,8 +1,10 @@
+import { TbMoodSadDizzy } from "react-icons/tb";
 
 const ErrorPage = () => {
     return (
-        <div>
-            404 error
+        <div className="flex justify-center items-center flex-col h-[100vh] gap-8">
+            <p className="text-4xl text-red-600 font-bold">Something went wrong!</p>
+            <TbMoodSadDizzy className="text-8xl text-red-600"></TbMoodSadDizzy>
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,63 +1,87 @@
 import { useContext, useEffect, useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
-// import logo from "../assets/cineverse_logo.png"
 import { FaCirclePlay } from "react-icons/fa6";
 
 const Navbar = () => {
-    const [bgColor, setBgColor] = useState('bg-transparent');
-    const [fontColor, setFontColor] = useState('text-white');
-    const { user, setuser, logout } = useContext(AuthContext)
+  const [bgColor, setBgColor] = useState("bg-transparent");
+  const [fontColor, setFontColor] = useState("text-white");
+  const { user, setuser, logout } = useContext(AuthContext);
 
-    useEffect(() => {
-      const handleScroll = () => {
-        const scrollThreshold = window.innerHeight * 0.5;
-  
-        if (window.scrollY > scrollThreshold) {
-          setBgColor('bg-black');
-          setFontColor('text-white');
-        } else {
-          setBgColor('bg-transparent');
-          setFontColor('text-white');
-        }
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollThreshold = window.innerHeight * 0.5;
 
-    const handleLogout = () => {
-        logout()
-        .then(() => {
-            console.log("User signed out successfully.");
-            setuser(null);
-          })
-          .catch((error) => {
-            console.error("Error signing out:", error);
-          });
-    }
-    return (
-        <div className={`w-full px-[4%] py-[1.5rem] flex justify-between fixed top-0 z-50 ${bgColor} transition-colors duration-1000 ${fontColor} font-bold text-lg items-center`}>
-            <div className="w-14">
-                <FaCirclePlay className="text-4xl text-primary"></FaCirclePlay>
+      if (window.scrollY > scrollThreshold) {
+        setBgColor("bg-black");
+        setFontColor("text-white");
+      } else {
+        setBgColor("bg-transparent");
+        setFontColor("text-white");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleLogout = () => {
+    logout()
+      .then(() => {
+        console.log("User signed out successfully.");
+        setuser(null);
+      })
+      .catch((error) => {
+        console.error("Error signing out:", error);
+      });
+  };
+  if (user) console.log(user.photoURL);
+
+  return (
+    <div
+      className={`w-full px-[4%] py-[1.5rem] flex justify-between fixed top-0 z-50 ${bgColor} transition-colors duration-1000 ${fontColor} font-bold text-lg items-center`}
+    >
+      <div className="w-14">
+        <Link to={'/'}><FaCirclePlay className="text-5xl text-primary"></FaCirclePlay></Link>
+      </div>
+      <ul className="flex gap-8">
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/all-movies"}>All Movies</NavLink>
+        <NavLink to={"/add-movies"}>Add Movies</NavLink>
+        <NavLink to={"my-favourites"}>My Favourites</NavLink>
+      </ul>
+      <div>
+        {user ? (
+          <div className="flex gap-6">
+            <button onClick={handleLogout}>Logout</button>
+            <div className="relative group w-10 h-10">
+              <div
+                className={`rounded-full w-10 h-10 p-[2px] border-2 border-gray-600 bg-contain`}
+                style={{
+                  backgroundImage: `url(${
+                    user.photoURL || "../assets/login_page.png"
+                  })`,
+                }}
+              ></div>
+              <div className="absolute top-12 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-sm rounded-md py-1 px-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                {user.displayName.split(" ")[0] || "Guest User"}
+              </div>
             </div>
-            <ul className="flex gap-8">
-                <NavLink to={"/"}>Home</NavLink>
-                <NavLink to={"/all-movies"}>All Movies</NavLink>
-                <NavLink to={"/add-movies"}>Add Movies</NavLink>
-                <NavLink to={"my-favourites"}>My Favourites</NavLink>
-            </ul>
-            <div>
-                {
-                    user ? 
-                    <button onClick={handleLogout}>Logout</button>
-                    :
-                    <button><Link to={"/login"}>Login</Link></button>
-                }
-            </div>
-        </div>
-    );
+          </div>
+        ) : (
+          <div>
+            <button>
+              <Link to={"/login"}>Login</Link>
+            </button>
+            <button>
+              <Link to={"/register"}>Register</Link>
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
